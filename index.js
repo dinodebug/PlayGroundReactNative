85959932a3ce15e8da8d4be6a62c110720f07f32
Nhan NFQ <dinhnhan.nguyen@nfq.asia>
2021-01-09 11:03:12 +0700

    add demo headphone flatlist slider

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/MovieFlatlistAnimations';
+import App from './src/HeadphoneFlatlistSlider';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
