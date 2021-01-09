e62525aea4a7afa40e42b1d380113dd7756a8323
dinodebug <77097441+dinodebug@users.noreply.github.com>
2021-01-09 10:42:11 +0700

    add demo movie flatlist carousel animations at 60 fps (#4)
    Co-authored-by: Nhan NFQ <dinhnhan.nguyen@nfq.asia>

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/FlatlistStackCarousel';
+import App from './src/MovieFlatlistAnimations';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
